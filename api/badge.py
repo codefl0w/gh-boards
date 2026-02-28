af0e076f5540bf633ac8bf885c2653747c71621c
codefl0w <fl0w@protonmail.com>
2026-02-28 13:53:31 +0300

    Link

--- api/badge.py
+++ api/badge.py
@@ -42,7 +42,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # CORS
         allowed_origins = [
-            "codefl0w.xyz",
+            "https://codefl0w.xyz",
             "http://localhost:3000",
             "http://127.0.0.1:3000",
         ]
